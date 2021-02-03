87040e3372866e809a6322affe491f887c8b6c83
Philipp Rosenberger <p.rosenberger@kunbus.com>
2021-02-03 15:16:08 +0100

    Test atecc508a_serial() function
    Signed-off-by: Philipp Rosenberger <p.rosenberger@kunbus.com>

--- src/revpi-serial.c
+++ src/revpi-serial.c
@@ -1,6 +1,16 @@
+#include <stdint.h>
 #include <stdio.h>
+
+#include "atecc508a.h"
 
 int main(int argc, char *argv[])
 {
-	return 0;
+	const unsigned int dev_addr = 0x60;
+	int ret;
+	uint8_t serial[8];
+	ret = atecc508a_serial("/dev/i2c-1", dev_addr, serial);
+	printf("Serial: 0x%02X%02X%02X%02X%02X%02X%02X%02X\n",
+	       serial[0], serial[1], serial[2], serial[3], serial[4], serial[5],
+	       serial[6], serial[7]);
+	return ret;
 }
