c21c660d83c43e6ac44dca1d20f244825e63acbb
Philipp Rosenberger <p.rosenberger@kunbus.com>
2021-02-04 12:41:16 +0100

    Add serial2pw() to create a password from a given serial number.
    This function was taken with only minor changes from the original code.
    
    Signed-off-by: Philipp Rosenberger <p.rosenberger@kunbus.com>

--- src/revpi-serial.c
+++ src/revpi-serial.c
@@ -3,6 +3,34 @@
 
 #include "atecc508a.h"
 
+static void serial2pw(char * const pw, const uint8_t * const serial)
+{
+	uint8_t pos, pos2;
+	uint16_t data;
+	uint8_t salt[] = { 0x81, 0xae, 0xf2, 0x9d, 0x47, 0xd3 };
+	const char code[] = "hancvo714xqwelm289rtz0356sdfgybp";
+
+	for (int i = 0; i < 6; i++)
+	{
+		salt[i] ^= serial[i + 2];
+	}
+
+	/* reduce the length to 4 bytes */
+	salt[2] ^= salt[5];
+	salt[0] ^= salt[4];
+
+	for (int i = 0; i < 6; i++)
+	{
+		pos = (i * 5) / 8;
+		data = salt[pos] + (salt[pos + 1] << 8);
+		pos2 = (i * 5) % 8;
+		data >>= pos2;
+		data &= 0x001f;
+		pw[i] = code[data];
+	}
+	pw[6] = '\0';
+}
+
 int main(int argc, char *argv[])
 {
 	const unsigned int dev_addr = 0x60;
