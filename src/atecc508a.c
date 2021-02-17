30798610b5cad06b182ae78546460461e964e118
Philipp Rosenberger <p.rosenberger@kunbus.com>
2021-02-17 16:10:38 +0100

    Add and use dbg/warn/err_print() functions
    Signed-off-by: Philipp Rosenberger <p.rosenberger@kunbus.com>

--- src/atecc508a.c
+++ src/atecc508a.c
@@ -9,6 +9,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "debug.h"
+
 const char * const dev_path = "/dev/i2c-1";
 
 int atecc508a_serial()
@@ -17,17 +19,16 @@ int atecc508a_serial()
 	int ret;
 	fd = open(dev_path, O_RDWR);
 	if (fd < 0) {
-		fprintf(stderr, "ERROR: Can't open file `%s': %s\n", dev_path,
-		        strerror(errno));
+		err_print("Can't open file `%s': %s\n", dev_path,
+		          strerror(errno));
 		return -errno;
 	}
 
 	unsigned int addr = 0x60;
 	ret = ioctl(fd, I2C_SLAVE, addr);
 	if (ret < 0) {
-		fprintf(stderr,
-		        "ERROR: IOCTL 'I2C_SLAVE' (0x%04x) on file `%s' failed: %s\n",
-		        I2C_SLAVE, dev_path, strerror(errno));
+		err_print("IOCTL 'I2C_SLAVE' (0x%04x) on file `%s' failed: %s\n",
+		          I2C_SLAVE, dev_path, strerror(errno));
 		ret = -errno;
 		goto out;
 	}
---
+++ src/debug.h
@@ -0,0 +1,59 @@
+#ifndef REVPI_SERIAL_H
+#define REVPI_SERIAL_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+#if (!defined(NDEBUG))
+# define DEBUG 1
+#else
+# define DEBUG 0
+#endif
+
+typedef enum error_level {
+	LEVEL_DEBUG,
+	LEVEL_WARN,
+	LEVEL_ERROR
+} error_level_t;
+
+static void log_print(const error_level_t level, const char *fmt, ...)
+{
+	const char *lstr;
+	va_list args;
+	switch (level)
+	{
+	case LEVEL_DEBUG:
+		lstr = "DEBUG: ";
+		break;
+	case LEVEL_WARN:
+		lstr = "WARNING: ";
+		break;
+	case LEVEL_ERROR:
+		lstr = "ERROR: ";
+		break;
+	default:
+		lstr = "UNKNOWN: ";
+		break;
+	}
+
+	fflush(stdout);
+	flockfile(stderr);
+	fputs(lstr, stderr);
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+	fflush(stderr);
+	funlockfile(stderr);
+}
+
+#define dbg_print(...) \
+	do { if (DEBUG) log_print(LEVEL_DEBUG, ##__VA_ARGS__); } while (0)
+
+#define warn_print(...) \
+	log_print(LEVEL_WARN, ##__VA_ARGS__)
+
+
+#define err_print(...) \
+	log_print(LEVEL_ERROR, ##__VA_ARGS__)
+
+#endif /* INCLUDE GARD */
